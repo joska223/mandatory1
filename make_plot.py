84300ac30d8583515dc5c68dc965a9f6d2582651
Jon Skauli <jonskau@uio.no>
2026-09-18 13:50:35 +0200

    made gifs smaller

--- make_plot.py
+++ make_plot.py
@@ -1,9 +1,9 @@
 from Wave2D import Wave2D, Wave2D_Neumann
 
 a = Wave2D()
-ani = a.animate(N = 30, Nt = 100)
-ani.save("wave2d.gif", writer="pillow", fps=20)
+ani = a.animate(N = 30, Nt = 50)
+ani.save("wave2d.gif", writer="pillow", fps=20, dpi = 60)
 
 b = Wave2D_Neumann()
-ani = b.animate(N = 30, Nt = 100)
-ani.save("wave2d_Neumann.gif", writer="pillow", fps=20)
+ani = b.animate(N = 30, Nt = 50)
+ani.save("wave2d_Neumann.gif", writer="pillow", fps=20, dpi = 60)
